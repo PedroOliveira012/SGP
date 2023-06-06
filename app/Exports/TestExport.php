0e5889ac11b3d2ccad9994d98b8a4ac8f0199a55
PedroOliveira012 <pedro120403@gmail.com>
2023-06-06 11:04:39 -0300

    Atualizações na função de download excel
    - Utilização de query para filtrar registros no banco
    - Filtro de colunas relevantes para a análise dos dados

--- app/Exports/TestExport.php
+++ app/Exports/TestExport.php
@@ -2,16 +2,27 @@
 
 namespace App\Exports;
 
-use App\Models\User;
+use App\Models\Task;
+use App\Models\Project;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\FromQuery;
+use Maatwebsite\Excel\Concerns\Exportable;
 
-class TestExport implements FromCollection
+class TestExport implements FromQuery
 {
-    /**
-    * @return \Illuminate\Support\Collection
-    */
-    public function collection()
+    use Exportable;
+
+    protected $projectId;
+
+    public function __construct($projectId)
     {
-        return User::all();
+        $this->projectId = $projectId;
+    }
+
+    public function query()
+    {
+        return Task::query()
+        ->select('painel', 'funcionario', 'inicio_tarefa', 'termino_tarefa', 'tempo_total')
+        ->where('id_projeto', $this->projectId);
     }
 }
--- app/Http/Controllers/ExcelController.php
+++ app/Http/Controllers/ExcelController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Project;
 use App\Exports\TestExport;
 use Maatwebsite\Excel\Facades\Excel;
@@ -12,8 +11,6 @@ class ExcelController extends Controller
     public function export($id){
         $projeto = Project::find($id);
         $nomeArquivo = strval($projeto->num_projeto) . '.xlsx';
-        return Excel::download(new TestExport, $nomeArquivo);
+        return Excel::download(new TestExport($projeto->id), $nomeArquivo);
     }
-
-
 }
