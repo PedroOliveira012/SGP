69875727e4e0c53c6ed2e83bdb5914bc14d1bfc8
PedroOliveira012 <pedro120403@gmail.com>
2023-06-12 09:58:36 -0300

    Colocar titulo nas colunas

--- app/Exports/TestExport.php
+++ app/Exports/TestExport.php
@@ -3,12 +3,17 @@
 namespace App\Exports;
 
 use App\Models\Task;
-use App\Models\Project;
-use Maatwebsite\Excel\Concerns\FromCollection;
+// use App\Models\Project;
+// use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\FromQuery;
 use Maatwebsite\Excel\Concerns\Exportable;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Concerns\WithEvents;
+use PhpOffice\PhpSpreadsheet\Style\Fill;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 
-class TestExport implements FromQuery
+
+class TestExport implements FromQuery, ShouldAutoSize, WithEvents, WithHeadings
 {
     use Exportable;
 
@@ -19,10 +24,20 @@ class TestExport implements FromQuery
         $this->projectId = $projectId;
     }
 
-    public function query()
-    {
+    public function query(){
         return Task::query()
         ->select('painel', 'funcionario', 'inicio_tarefa', 'termino_tarefa', 'tempo_total')
         ->where('id_projeto', $this->projectId);
+    }
+
+    public function headings(): array
+    {
+        return [
+            'Painel',
+            'Funcionário',
+            'Início da Tarefa',
+            'Término da Tarefa',
+            'Tempo Total',
+        ];
     }
 }
