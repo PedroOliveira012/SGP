5dfc3952323729a6ee8690cf59649780379d8912
PedroOliveira012 <pedro120403@gmail.com>
2023-06-13 08:25:39 -0300

    Planilha e nova tarefa
    - Estilização da planilha
    - Adição de uma tarefa nova

--- app/Exports/TestExport.php
+++ app/Exports/TestExport.php
@@ -4,17 +4,19 @@ namespace App\Exports;
 
 use App\Models\Task;
 // use App\Models\Project;
-// use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\FromQuery;
 use Maatwebsite\Excel\Concerns\Exportable;
 use Maatwebsite\Excel\Concerns\ShouldAutoSize;
 use Maatwebsite\Excel\Concerns\WithEvents;
-use PhpOffice\PhpSpreadsheet\Style\Fill;
 use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithCustomStartCell;
+use Maatwebsite\Excel\Concerns\WithMapping;
+use Maatwebsite\Excel\Events\AfterSheet;
+use PhpOffice\PhpSpreadsheet\Style\Fill;
 
 
-class TestExport implements FromQuery, ShouldAutoSize, WithEvents, WithHeadings
-{
+class TestExport implements FromQuery, ShouldAutoSize, WithEvents, WithHeadings{
     use Exportable;
 
     protected $projectId;
@@ -39,5 +41,69 @@ class TestExport implements FromQuery, ShouldAutoSize, WithEvents, WithHeadings
             'Término da Tarefa',
             'Tempo Total',
         ];
+    }
+
+    // public function startCell(): string
+    // {
+    //     return 'G1'; // Define a célula de início para B2
+    // }
+
+    // public function registerEvents(): array
+    // {
+    //     // return [
+    //     //     AfterSheet::class => function (AfterSheet $event) {
+    //     //         $sheet = $event->sheet;
+
+    //     //         // Estilize a célula B2 como exemplo
+    //     //         $sheet->getStyle('B2')->applyFromArray([
+    //     //             'font' => [
+    //     //                 'bold' => true,
+    //     //                 'color' => ['rgb' => 'FF0000'], // Fonte em vermelho
+    //     //             ],
+    //     //         ]);
+    //     //     },
+    //     // ];
+    // }
+    public function registerEvents(): array
+    {
+        return [
+            AfterSheet::class => function (AfterSheet $event) {
+                $sheet = $event->sheet;
+                $highestRow = $sheet->getHighestRow();
+                // Estilo para a primeira linha (cabeçalho)
+                $sheet->getStyle('A1:E1')->applyFromArray([
+                    'font' => [
+                        'bold' => true,
+                        'color' => ['rgb' => 'FFFFFF'],
+                    ],
+                    'fill' => [
+                        'fillType' => Fill::FILL_SOLID,
+                        'startColor' => [
+                            'rgb' => '000000',
+                        ],
+                    ],
+                ]);
+                for ($row = 2; $row <= $highestRow; $row++) {
+                    $style = ($row % 2 == 0) ? '4bacc6' : '31869b';
+
+                    $sheet->getStyle('A' . $row . ':E' . $row)->applyFromArray([
+                        'fill' => [
+                            'fillType' => Fill::FILL_SOLID,
+                            'startColor' => [
+                                'rgb' => $style,
+                            ],
+                        ],
+                        'font' => [
+                            'color' => ['rgb' => 'FFFFFF'],
+                        ],
+                    ]);
+                }
+            },
+        ];
+    }
+
+    public function startCell(): string
+    {
+        return 'G1'; // Define a célula de início para B2
     }
 }
