c74b949a891a3245a65d4ac8d6ee083bce7199dd
PedroOliveira012 <pedro120403@gmail.com>
2025-05-12 10:36:55 -0300

    Alterações nas migrations
    - Remoção das não necessárias e nova migration para excluir colunas do banco

---
+++ database/migrations/2025_05_12_131541_remover_coluna_projeto.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class RemoverColunaProjeto extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('projects', function (Blueprint $table) {
+            $table->dropColumn("Responsavel_tecnico");
+            $table->dropColumn("valor_contratado");
+            $table->dropColumn("prazo_entrega");
+            $table->dropColumn("observacoes");
+            $table->dropColumn("liberado");
+            $table->dropColumn("fase_teste");
+            $table->dropColumn("finalizado");
+            $table->dropColumn("responsavel");
+            $table->dropColumn("created_at");
+            $table->dropColumn("updated_at");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('projects', function (Blueprint $table) {
+            //
+        });
+    }
+}
