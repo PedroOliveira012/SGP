49b9351d89b46a6b3520f0b7b0d845affba473c8
PedroOliveira012 <pedro120403@gmail.com>
2025-07-14 15:54:39 -0300

    Update no form de registro de usuario
    - Mudança de email para username no label do register e no input para aceitar text e não restrito ao email
    - Alteração no validade do controller para nao aceitar somente email e passar a aceitar uma string

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -36,9 +36,9 @@
 
             <!-- Email Address -->
             <div class="mt-4">
-                <x-label for="email" :value="__('Email')" />
+                <x-label for="email" :value="__('Username')" />
 
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
+                <x-input id="email" class="block mt-1 w-full" type="text" name="email" required />
             </div>
 
             <!-- Password -->
